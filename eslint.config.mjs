659bae2cee4d685c9e321feb7b229cbcb6a156d0
Martins Gouveia <gouveiamartins6@gmail.com>
2025-04-01 10:18:10 +0100

    feat: update ESLint configuration to customize rules for console usage and variable handling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		rules: {
+			"no-console": "warn", // Example: Warn when console statements are used
+			"no-unused-vars": "off", // Example: Warn when there are unused variables
+			"@typescript-eslint/no-unused-vars": "off", // Example: Warn when there are unused variables
+			"@typescript-eslint/no-explicit-any": "off", // Example: Allow the use of 'any' type
+		},
+	},
 ];
 
 export default eslintConfig;
