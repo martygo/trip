26960c212b9f53e4421b40f43a839a4f71632ad4
Martins Gouveia <gouveiamartins6@gmail.com>
2025-04-10 09:53:33 +0100

    feat: add logo image to header and footer components

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,15 +1,27 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Footer() {
 	return (
 		<footer className="bg-green-950 text-white py-6">
 			<div className="container">
-				<div className="flex justify-between">
-					<p className="text-[18px]">
-						&copy; {new Date().getFullYear()}{" "}
-						<span className="font-semibold">SmartTrip.</span>
-					</p>
-					<p className="text-[18px]">Contact: +92 932434349</p>
+				<div className="flex items-center justify-between">
+					<div className="flex items-center gap-2">
+						<Image
+							src="/tes.jpg"
+							alt="Logo"
+							width={50}
+							height={50}
+							className="rounded-md object-contain"
+						/>
+						<p className="text-[18px]">
+							&copy; {new Date().getFullYear()}{" "}
+							<span className="font-semibold">SmartTrip.</span>
+						</p>
+					</div>
+					<div className="flex">
+						<p className="text-[18px]">Contact: +92 932434349</p>
+					</div>
 				</div>
 			</div>
 		</footer>
--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/shared/theme-toggle";
@@ -13,9 +14,18 @@ export default function Header() {
 	const { isAuthenticated } = useAuth();
 
 	return (
-		<header className="sticky top-0 z-20 flex justify-between items-center p-4 bg-green-600 border-b border-green-700">
-			<div className="text-xl font-bold text-white">
-				<Link href="/">SmartTrip</Link>
+		<header className="sticky top-0 z-20 flex justify-between items-center p-4 bg-green-400">
+			<div className="flex flex-row-reverse items-center gap-2">
+				<Link href="/" className="text-xl font-bold text-white">
+					SmartTrip
+				</Link>
+				<Image
+					src="/tes.jpg"
+					alt="Logo"
+					width={50}
+					height={50}
+					className="rounded-md object-contain"
+				/>
 			</div>
 			<div className="flex items-center gap-4">
 				<ThemeToggle />
